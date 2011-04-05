bee9e68751f09eceecf5832ef4c988de735787f1
Timothy Chen <tnachen@gmail.com>
2011-04-04 17:17:57 -0700

    Adding emacs extensions

---
+++ emacs/site-start.el
@@ -0,0 +1,19 @@
+(require 'whitespace)
+
+(when (cdr command-line-args)
+  (setcdr command-line-args (cons "--no-splash" (cdr command-line-args))))
+
+;; Load ruby mode when needed
+(autoload 'ruby-mode "ruby-mode" "Ruby mode" t )
+;; Assign .rb and .rake files to use ruby mode
+(setq auto-mode-alist (cons '("\\.rb\\'" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.rake\\'" . ruby-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+;; Show syntax highlighting when in ruby mode
+(add-hook 'ruby-mode-hook '(lambda () (font-lock-mode 1)))
+
+;; Set the default tab width to 4
+(setq-default tab-width 4)
